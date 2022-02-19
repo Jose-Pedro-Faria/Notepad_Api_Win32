08d0c6a50f8e72bce2c5a0315c16d127557e9678
José Pedro Faria <josepedropfaria@gmail.com>
2022-02-19 02:36:37 +0000

    Menus/TaskBar

--- Notepad-Win32/main.cpp
+++ Notepad-Win32/main.cpp
@@ -1,8 +1,15 @@
 #include <windows.h>
 
+#define FILE_MENU_NOVO 1
+#define FILE_MENU_ABRIR 2
+#define FILE_MENU_SAIR 3
 LRESULT CALLBACK WindowsProcedure(HWND,UINT,WPARAM,LPARAM);
 
-int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args,int ncmdshow)         //
+void AddMenus(HWND);     //declaração função adicionar menus
+
+HMENU hMenu;
+
+int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args,int ncmdshow)         //função principal
 {
     WNDCLASSW wc = {0};     //Class wc de Windows Class
 
@@ -15,7 +22,7 @@ int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args,int ncmdsho
     if (!RegisterClassW(&wc))                       //
         return -1;
 
-    CreateWindowW(L"WindowClass", L"Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 600,300,1000,600,NULL,NULL,NULL,NULL);
+    CreateWindowW(L"WindowClass", L"Notepad do Faria", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 600,300,1000,600,NULL,NULL,NULL,NULL);
 
     MSG msg = {0};
 
@@ -33,6 +40,23 @@ LRESULT CALLBACK WindowsProcedure(HWND hWnd,UINT msg,WPARAM wp,LPARAM lp)
 {
     switch (msg)
     {
+    case WM_COMMAND:
+        switch(wp)
+        {
+        case FILE_MENU_SAIR:
+            DestroyWindow(hWnd);        //Se alguem clicar "Sair" a janela fecha e a aplicação encerra
+            break;
+        case FILE_MENU_NOVO:
+            break;
+        case FILE_MENU_ABRIR:
+            break;
+        }
+
+
+        break;
+    case WM_CREATE:
+        AddMenus(hWnd);
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -40,3 +64,30 @@ LRESULT CALLBACK WindowsProcedure(HWND hWnd,UINT msg,WPARAM wp,LPARAM lp)
         return DefWindowProcW(hWnd,msg,wp,lp);
     }
 }   //fim WindowsProcesure
+
+
+void AddMenus(HWND hWnd)         //Função criar menu "taskbar"
+{
+    hMenu = CreateMenu();
+    HMENU hFileMenu = CreateMenu();         //Criar Menu
+    HMENU hSubMenu = CreateMenu();          //Criar SubMenu
+
+    AppendMenu(hSubMenu,MF_STRING,NULL,"SubMenu");
+
+    AppendMenu(hFileMenu,MF_STRING,FILE_MENU_NOVO,"Novo");          //SubMenu de File -> Menu NOVO
+    AppendMenu(hFileMenu,MF_POPUP,(UINT_PTR)hSubMenu,"Abrir");      //SubMenu de File -> Menu Abrir -> com submenu SubMenu
+    AppendMenu(hFileMenu,MF_SEPARATOR,NULL,NULL);                   //SubMenu de File -> Separador de divisão so submenu de File
+    AppendMenu(hFileMenu,MF_STRING,FILE_MENU_SAIR,"Sair");          //SubMenu de File -> Menu Sair //fecha o programa
+
+    AppendMenu(hMenu,MF_POPUP,(UINT_PTR)hFileMenu,"Ficheiro");
+    AppendMenu(hMenu,MF_POPUP,NULL,"Editar");
+    AppendMenu(hMenu,MF_POPUP,NULL,"Formatar");
+    AppendMenu(hMenu,MF_POPUP,NULL,"Ver");
+    AppendMenu(hMenu,MF_POPUP,NULL,"Ajuda");
+
+    SetMenu(hWnd, hMenu);
+}
+
+
+
+
