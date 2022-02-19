c7bb072004b263591324748b59786c9b03f89efb
José Pedro Faria <josepedropfaria@gmail.com>
2022-02-19 01:59:10 +0000

    configuração janela principal

--- Notepad-Win32/main.cpp
+++ Notepad-Win32/main.cpp
@@ -1,14 +1,42 @@
 #include <windows.h>
 
+LRESULT CALLBACK WindowsProcedure(HWND,UINT,WPARAM,LPARAM);
 
-
-int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+int WINAPI WinMain(HINSTANCE hInst , HINSTANCE hPrevInst, LPSTR args,int ncmdshow)         //
 {
-    WNDCLASSW wc = (0);
+    WNDCLASSW wc = {0};     //Class wc de Windows Class
 
-    wc.hbrBackground = (HBRUSH) COLOR_WINDOW ;
-    wc.hCursor =
+    wc.hbrBackground = (HBRUSH) COLOR_WINDOW ;      //configuração Background
+    wc.hCursor = LoadCursor(NULL,IDC_ARROW);        //configuração Cursor
+    wc.hInstance = hInst;                           //configuração instancia
+    wc.lpszClassName = L"WindowClass";              //
+    wc.lpfnWndProc = WindowsProcedure;              //
+
+    if (!RegisterClassW(&wc))                       //
+        return -1;
+
+    CreateWindowW(L"WindowClass", L"Window", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 600,300,1000,600,NULL,NULL,NULL,NULL);
+
+    MSG msg = {0};
+
+    while(GetMessage(&msg,NULL,NULL,NULL))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
 
     return 0;
-}
+}   //fim main
+
+LRESULT CALLBACK WindowsProcedure(HWND hWnd,UINT msg,WPARAM wp,LPARAM lp)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProcW(hWnd,msg,wp,lp);
+    }
+}   //fim WindowsProcesure
